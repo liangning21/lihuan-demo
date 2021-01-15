3e6e78163431ed8e7e40790068fb6647e209d16d
dell <dell>
2021-01-15 15:16:07 +0800

    分支的

--- lihuan-demo-zhengze/src/main/java/cn/lihuan/com/utils/RegExpUtils.java
+++ lihuan-demo-zhengze/src/main/java/cn/lihuan/com/utils/RegExpUtils.java
@@ -1,5 +1,7 @@
 package cn.lihuan.com.utils;
 
+import cn.lihuan.com.RegExpEnum;
+
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -34,6 +36,54 @@ public class RegExpUtils {
         }
         return stringBuilder.toString();
     }
+    //匹配银行卡
+    public static String demo3(String result) {
+        StringBuilder stringBuilder = new StringBuilder();
+        String regex ="[0-9]\\d*";
+        Pattern p = Pattern.compile(regex);
+        Matcher m=p.matcher(result);
+        System.out.println("读取出所有的数字");
+        while(m.find()){
+            String number = m.group();
+            //判断这个数字是否属于银行卡号
+            Pattern pattern = Pattern.compile(RegExpEnum.BANK_CARD_RegExp.getValue());
+            Matcher matcher = pattern.matcher(number);
+            if (matcher.find()) {
+                stringBuilder.append(matcher.group()+",");
+                System.out.println("银行卡号 = " + matcher.group());
+            }
+        }
+        return stringBuilder.toString();
+    }
 
-
+    //匹配身份证
+    public static String demo4(String result) {
+        StringBuilder stringBuilder = new StringBuilder();
+        String regex ="[0-9]\\d*";
+        Pattern p = Pattern.compile(regex);
+        Matcher m=p.matcher(result);
+        System.out.println("读取出所有的数字");
+        while(m.find()){
+            String number = m.group();
+            //判断这个数字是否属于身份证号
+            Pattern pattern = Pattern.compile(RegExpEnum.IDENTITY_CARD_RegExp.getValue());
+            Matcher matcher = pattern.matcher(number);
+            if (matcher.find()) {
+                stringBuilder.append(matcher.group()+",");
+                System.out.println("身份证号 = " + matcher.group());
+            }
+        }
+        return stringBuilder.toString();
+    }
+    //匹配日期  yyyy-MM-dd  yyyy.MM.dd
+    public static String demo5(String result){
+        StringBuilder stringBuilder = new StringBuilder();
+        Pattern compile = Pattern.compile(RegExpEnum.DATA_RegExp.getValue());
+        Matcher matcher = compile.matcher(result);
+        while (matcher.find()){
+            stringBuilder.append(matcher.group()+",");
+            System.out.println("日期时间为 = " + matcher.group());
+        }
+        return stringBuilder.toString();
+    }
 }
